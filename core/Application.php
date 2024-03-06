85c6322e9b2fe7c8ae108ad0a0daca6c8f74cd32
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-06 17:22:28 +0530

    Initial Commit

---
+++ core/Application.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\core;
+
+/**
+ * Class Application
+ * @author Ravi Kishan <@Ravikisha> <ravikishan63392@gmail.com>
+ * @package core
+ * @property Router $router
+ */
+
+class Application {
+    public Router $router;
+    
+}
---
+++ core/Router.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Class Application
+ * @author Ravi Kishan <@Ravikisha> <ravikishan63392@gmail.com>
+ * @package app\core
+ */
+
+namespace app\core;
+
+class Router {
+    
+}
