640af37b903beaea980b973b9b902c50be21cb6b
ravikishan.69 <ravikishan63392@gmail.com>
2024-03-08 18:40:03 +0530

    Working on docs of Controller

--- core/Controller.php
+++ core/Controller.php
@@ -4,14 +4,22 @@ namespace App\core;
 
 class Controller
 {
-
+    /**
+     * Set the layout for the view
+     * @var string
+     */
     public string $layout = 'main';
-
     public function setLayout($layout)
     {
         $this->layout = $layout;
     }
 
+    /**
+     * Render the view with the layout
+     * @param string $view
+     * @param array $params
+     * @return string
+     */
     public function render($view, $params = [])
     {
         return Application::$app->router->renderView($view, $params);
